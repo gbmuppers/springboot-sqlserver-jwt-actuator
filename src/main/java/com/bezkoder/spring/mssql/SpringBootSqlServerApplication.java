28b0040ea156f2dce4dbeef79d57a07f9d265430
Gibran Ojeda <gibran.muppers@gmail.com>
2022-09-04 20:42:58 -0500

    Allow cross origin

--- src/main/java/com/bezkoder/spring/mssql/SpringBootSqlServerApplication.java
+++ src/main/java/com/bezkoder/spring/mssql/SpringBootSqlServerApplication.java
@@ -1,17 +1,28 @@
 package com.bezkoder.spring.mssql;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.bezkoder.spring.mssql.security.JWTAuthorizationFilter;
+
+
 
 @SpringBootApplication
 public class SpringBootSqlServerApplication extends SpringBootServletInitializer{
@@ -31,12 +42,20 @@ public class SpringBootSqlServerApplication extends SpringBootServletInitializer
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
+			http.cors().configurationSource(request -> {
+				CorsConfiguration cors = new CorsConfiguration();
+				cors.setAllowedOrigins(Arrays.asList("*"));
+				cors.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
+				cors.setAllowedHeaders(Arrays.asList("*"));
+				return cors;
+			  }).and()
+				.csrf().disable()
 				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
 				.authorizeRequests()
 				.antMatchers(HttpMethod.POST, "/auth").permitAll()
 				.anyRequest().authenticated();
 		}
+
 	}
 
 }
--- src/main/java/com/bezkoder/spring/mssql/controller/TutorialController.java
+++ src/main/java/com/bezkoder/spring/mssql/controller/TutorialController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bezkoder.spring.mssql.model.TcGruposDiscriminados;
 import com.bezkoder.spring.mssql.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
+//@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
